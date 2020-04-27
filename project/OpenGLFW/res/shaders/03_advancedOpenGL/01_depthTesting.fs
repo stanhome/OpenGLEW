34193f243a8f0ed00a471e75058bc021339f19c6
Stan <stancai@dianhun.cn>
2020-04-27 18:57:27 +0800

    - depth testing for depth visualizing

---
+++ project/OpenGLFW/res/shaders/03_advancedOpenGL/01_depthTesting.fs
@@ -0,0 +1,27 @@
+#version 330 core
+in vec2 texCoords;
+
+out vec4 FragColor;
+
+uniform sampler2D _mainTex;
+
+float near = 0.1;
+float far = 100.0f;
+
+float LinearizeDepth(float depth)
+{
+	float z = depth * 2.0 - 1.0; // back to NDC
+	return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
+
+void main()
+{
+	// non-linear
+	// FragColor = vec4(vec3(gl_FragCoord.z), 1.0f);
+
+	// linear depth
+	// divide by far for demonstration
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; 
+    FragColor = vec4(vec3(depth), 1.0);
+}
