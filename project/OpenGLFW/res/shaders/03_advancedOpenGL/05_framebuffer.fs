856138b71431cb8ff5b1f0d0eac03991436ec13e
Stan <stancai@dianhun.cn>
2020-04-30 19:06:10 +0800

    -added post process: grayscale

--- project/OpenGLFW/res/shaders/03_advancedOpenGL/05_framebuffer.fs
+++ project/OpenGLFW/res/shaders/03_advancedOpenGL/05_framebuffer.fs
@@ -9,5 +9,14 @@ void main()
 {
 	vec3 texColor = texture(screenTexture, texCoords).rgb;
 	// FragColor = vec4(vec3(texColor), 1.0f);
-	FragColor = vec4(vec3(1.0 - texColor), 1.0f);
+
+	// PostProcess:
+	//-------------------
+	// 1. inversion
+	// FragColor = vec4(vec3(1.0 - texColor), 1.0f);
+
+	// 2. Grayscale
+	const vec3 grayFactor = vec3(0.2126, 0.7152, 0.0722);
+	float average = dot(texColor, grayFactor);
+	FragColor = vec4(average, average, average, 1.0f);
 }
