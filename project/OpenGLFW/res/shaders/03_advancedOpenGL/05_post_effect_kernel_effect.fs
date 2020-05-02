e6bc02c7938f7bf50da66532fa3ff50401fbca1f
stan <feihu_guest@qq.com>
2020-05-02 22:46:13 +0800

    - added Blur post process

--- project/OpenGLFW/res/shaders/03_advancedOpenGL/05_post_effect_kernel_effect.fs
+++ project/OpenGLFW/res/shaders/03_advancedOpenGL/05_post_effect_kernel_effect.fs
@@ -21,10 +21,18 @@ void main()
 		vec2( offset, -offset)  // bottom-right  
 	);
 
+	// particular sharpen
+	// float kernel[9] = float[](
+    //     -1, -1, -1,
+    //     -1,  9, -1,
+    //     -1, -1, -1
+	// );
+
+	// Blur
 	float kernel[9] = float[](
-        -1, -1, -1,
-        -1,  9, -1,
-        -1, -1, -1
+		1.0 / 16, 2.0 / 16, 1.0 / 16,
+		2.0 / 16, 4.0 / 16, 2.0 / 16,
+		1.0 / 16, 2.0 / 16, 1.0 / 16  
 	);
 
 	vec3 sampleTex[9];
